3556f797656b77e6becba77ce8bc615dd902f39f
estacioj91 <estacioj91@yahoo.com>
2018-08-22 14:07:06 -0700

    practice with event handlers
    practice with event handlers

---
+++ js/notes.js
@@ -0,0 +1,18 @@
+const notes = [
+    {
+        title: "my next trip",
+        body: "I would like to go to spain"
+    },
+    {
+        title: "Habbits to work on",
+        body: "Eat better"
+    },
+    {
+        title: "Office Modification",
+        body: "Get a new seat"
+    },
+]
+
+document.querySelector("button").addEventListener("click", function(event){
+    console.log(event);
+})
