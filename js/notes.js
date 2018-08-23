5fa1d3e2a756e075b7046e0c896195d3d3434a3a
estacioj91 <estacioj91@yahoo.com>
2018-08-22 19:12:29 -0700

    Render filter data
    default data is rendered according to filter input

--- js/notes.js
+++ js/notes.js
@@ -1,3 +1,4 @@
+//test notes
 const notes = [
     {
         title: "my next trip",
@@ -12,7 +13,38 @@ const notes = [
         body: "Get a new seat"
     },
 ]
+//filter string
+const filters = {
+    searchText: ""
+}
+//renders notes initially with a default empty filter
+//gets called later on in an event listener if input string 
+//changes
+const renderNotes = function (notes, filters){
+    const filteredNotes = notes.filter(function(note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
+    })
+    document.querySelector("#notes").innerHTML = "";
+    filteredNotes.forEach(function(note){
+        const noteElement = document.createElement('p');
+        noteElement.textContent  = note.title;
+        document.querySelector("#notes").appendChild(noteElement);
+    })
+}
+//inital call
+renderNotes(notes,filters);
 
-document.querySelector("button").addEventListener("click", function(event){
+document.getElementById("create-note").addEventListener("click", function (event) {
     console.log(event);
 })
+document.getElementById("remove-all").addEventListener("click", function (event) {
+    document.querySelectorAll(".note").forEach(function(note){
+        note.remove();
+    });
+})
+//listens to changes in input field
+document.querySelector("#search-text").addEventListener("input", function(event){
+    console.log(event.target.value);
+    filters.searchText = event.target.value;
+    renderNotes(notes,filters);
+});
