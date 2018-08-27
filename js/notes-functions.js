dc1bb2f079454c4557eac7dcaf1fe32806d5797e
estacioj91 <estacioj91@yahoo.com>
2018-08-26 22:18:49 -0700

    added full functionality
    all basic functionalities added

--- js/notes-functions.js
+++ js/notes-functions.js
@@ -13,18 +13,84 @@ const getSavedNotes = function () {
 const saveNotes = function(notes){
     localStorage.setItem("notes", JSON.stringify(notes));
 }
+//note remove function
+const removeNote = function(id){
+    const noteIndex = notes.findIndex(function(note){
+        return note.id === id;
+    });
+    if(noteIndex > -1){
+        notes.splice(noteIndex, 1);
+    }
+}
 //creates dom structure for a note
 const generateNoteDOM = function (note) {
-    const noteElement = document.createElement('p');
+    const noteElement = document.createElement("div");
+    const textElement = document.createElement('a');
+    const button = document.createElement("button");
+
+    button.addEventListener("click", function(event){
+        removeNote(note.id);
+        saveNotes(notes);
+        renderNotes(notes,filters)
+    })
+    //creates button
+    button.textContent = "x";
+    noteElement.appendChild(button);
+    //creates note title
     if (note.title.length > 0) {
-        noteElement.textContent = note.title;
+        textElement.textContent = note.title;
     } else {
-        noteElement.textContent = "Unnamed note"
+        textElement.textContent = "Unnamed note"
     }
+    textElement.setAttribute("href", `/edit.html#${note.id}`);
+    noteElement.appendChild(textElement);
     return noteElement;
+}
+//sort function
+const sortNotes = function(notes, sortBy){
+    if(sortBy ==="byEdited"){
+        return notes.sort(function(a,b){
+            if(a.updatedAt> b.updatedAt){
+                return -1;
+            }
+            else if( a.updatedAt< b.updatedAt){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        });
+    }
+    else if(sortBy ==="byCreated"){
+        return notes.sort(function(a,b){
+            if(a.createdAt> b.createdAt){
+                return -1;
+            }
+            else if( a.createdAt< b.createdAt){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        });
+    }
+    else if(sortBy ==="alphabetical"){
+        return notes.sort(function(a,b){
+            if(a.title.toLowerCase()> b.title.toLowerCase()){
+                return 1;
+            }
+            else if( a.title.toLowerCase()< b.title.toLowerCase()){
+                return -1;
+            }
+            else{
+                return 0;
+            }
+        });
+    }
 }
 //Render application notes
 const renderNotes = function (notes, filters) {
+    notes = sortNotes(notes,filters.sortBy);
     const filteredNotes = notes.filter(function (note) {
         return note.title.toLowerCase().includes(filters.searchText.toLowerCase());
     })
@@ -33,4 +99,7 @@ const renderNotes = function (notes, filters) {
         const noteElement = generateNoteDOM(note);
         document.querySelector("#notes").appendChild(noteElement);
     })
+}
+const generateLastEdited = function(timeStamp){
+    return `Last edited ${moment(note.updatedAt).fromNow()}`;
 }
--- js/notes.js
+++ js/notes.js
@@ -1,18 +1,23 @@
-const notes = getSavedNotes();
+let notes = getSavedNotes();
 
 const filters = {
-    searchText: ""
+    searchText: "",
+    sortBy: "byEdited"
 }
 //inital call
 renderNotes(notes, filters);
-
 document.getElementById("create-note").addEventListener("click", function (event) {
+    const id = uuidv4();
+    const timeStamp = moment().valueOf();
     notes.push({
+        createdAt: timeStamp,
+        updatedAt: timeStamp,
+        id: id,
         title: "",
         body: ""
     });
     saveNotes(notes);
-    renderNotes(notes, filters);
+    location.assign(`/edit.html#${id}`);
 })
 //listens to changes in input field
 document.querySelector("#search-text").addEventListener("input", function (event) {
@@ -21,5 +26,14 @@ document.querySelector("#search-text").addEventListener("input", function (event
     renderNotes(notes, filters);
 });
 document.querySelector("#filter-by").addEventListener("change", function (event) {
-
+    filters.sortBy = event.target.value;
+    renderNotes(notes,filters);
 })
+window.addEventListener("storage", function (event) {
+    if (event.key === "notes") {
+        notes = JSON.parse(event.newValue);
+        renderNotes(notes, filters);
+    }
+})
+const now = moment();
+console.log(now.toString());
