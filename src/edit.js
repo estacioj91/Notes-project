bdec6496acb06e3f908ae2def23f30699bccee59
estacioj91 <estacioj91@yahoo.com>
2018-09-06 20:57:02 -0700

    final change
    change

--- src/edit.js
+++ src/edit.js
@@ -1,9 +1,10 @@
-import {initializeEditPage} from "./views"
-import { updateNote } from "./notes";
+import {initializeEditPage, generateLastEdited} from "./views"
+import { updateNote, removeNote,  } from "./notes";
 
 const titleElement = document.querySelector("#note-title");
 const bodyElement = document.querySelector("#note-body");
 const dateElement = document.querySelector("#last-edited");
+const removeElement = document.querySelector("#remove-note");
 const noteId = location.hash.substring(1);
 
 initializeEditPage(noteId);
@@ -21,8 +22,7 @@ bodyElement.addEventListener("input", (event) => {
     dateElement.textContent = generateLastEdited(note.updatedAt);
 })
 removeElement.addEventListener("click", (event) => {
-    removeNote(note.id);
-    saveNotes(notes);
+    removeNote(noteId);
     location.assign("/index.html");
 })
 window.addEventListener("storage", (event) => {
