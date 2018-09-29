2523b5da6a4dff9697a3d4f894c7e05fb3e6d2fb
estacioj91 <estacioj91@yahoo.com>
2018-09-28 19:11:55 -0700

    change to docs

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
         edit: ["babel-polyfill", "./src/edit.js"]
     },
     output: {
-        path: path.resolve(__dirname, "public/scripts"),
+        path: path.resolve(__dirname, "docs/scripts"),
         filename: "[name]-bundle.js"
     },
     module: {
@@ -21,7 +21,7 @@ module.exports = {
         }]
     },
     devServer: {
-        contentBase: path.resolve(__dirname, "public"),
+        contentBase: path.resolve(__dirname, "docs"),
         publicPath: "/scripts/"
     },
     devtool: "source-map"
